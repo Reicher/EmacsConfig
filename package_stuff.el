8f4045a973039aff4248286bc86e0bdf9e5f2338
Robin Reicher <robin.o.reicher@gmail.com>
2017-03-14 16:24:46 +0100

    forgot file...

---
+++ package_stuff.el
@@ -0,0 +1,20 @@
+;;;; Setup so we can use package management system and use-package(require 'package)
+(progn  (setq package-archives nil)
+	(add-to-list 'package-archives
+		     '("melpa" . "https://melpa.org/packages/"))
+	(add-to-list 'package-archives
+		     '("org" . "http://orgmode.org/elpa/")))
+(package-initialize)
+
+;; If never connected to repositories before, download package descriptions so
+;;`use-package' can trigger installation of missing packages.
+(unless package-archive-contents
+  (message "Refreshing ELPA package archives...")
+  (package-refresh-contents))
+
+;; ...but before everything, make sure `use-package' is installed.
+(unless (package-installed-p 'use-package)
+  (message "`use-package' not found. Installing...")
+  (package-install 'use-package))
+
+(require 'use-package)
